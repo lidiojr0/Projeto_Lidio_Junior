f1878d17580d93e8ff0d51f5efeb1d133f2aab88
Lidio Jr <lidiojunior1234567tn@gmail.com>
2025-09-21 18:44:04 -0300

    Criação das classes A e B em C++ com métodos e atributos

---
+++ A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class A {
+private:
+    int A1;
+    float A2;
+
+public:
+    // Getters e Setters
+    int getA1() { return A1; }
+    void setA1(int value) { A1 = value; }
+
+    float getA2() { return A2; }
+    void setA2(float value) { A2 = value; }
+
+    // Métodos
+    void MA1() {
+        cout << "Método MA1 executado" << endl;
+    }
+
+    void MA2() {
+        cout << "Método MA2 executado" << endl;
+    }
+};
+
+int main() {
+    A obj;
+    obj.setA1(10);
+    obj.setA2(3.14);
+    obj.MA1();
+    obj.MA2();
+    return 0;
+}
---
+++ B.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class B {
+private:
+    int B1;
+    float B2;
+
+public:
+    // Getters e Setters
+    int getB1() { return B1; }
+    void setB1(int value) { B1 = value; }
+
+    float getB2() { return B2; }
+    void setB2(float value) { B2 = value; }
+
+    // Métodos
+    void MB1() {
+        cout << "Método MB1 executado" << endl;
+    }
+
+    void MB2() {
+        cout << "Método MB2 executado" << endl;
+    }
+};
+
+int main() {
+    B obj;
+    obj.setB1(20);
+    obj.setB2(6.28);
+    obj.MB1();
+    obj.MB2();
+    return 0;
+}
